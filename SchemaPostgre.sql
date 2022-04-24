afde573fd6a48a296027e966e617a84d89309caa
LuizGBFPerez <102800213+LuizGBFPerez@users.noreply.github.com>
2022-04-24 20:58:01 -0300

    Update and rename schema.sql to SchemaPostgre.sql

--- SchemaPostgre.sql
+++ SchemaPostgre.sql
@@ -1,5 +1,6 @@
 
-CREATE TABLE elmasri.funcionario (
+CREATE TABLE elmasri.funcionario
+             (
                 cpf CHAR(11) NOT NULL,
                 primeiro_nome VARCHAR(15) NOT NULL,
                 nome_meio CHAR(1),
@@ -11,7 +12,7 @@ CREATE TABLE elmasri.funcionario (
                 cpf_supervisor CHAR(11) NOT NULL,
                 numero_departamento INTEGER NOT NULL,
                 CONSTRAINT cpf_pk PRIMARY KEY (cpf)
-);
+              );
 COMMENT ON TABLE elmasri.funcionario IS 'Tabela que armazena as informações dos funcionários.';
 COMMENT ON COLUMN elmasri.funcionario.cpf IS 'CPF do funcionário. É a PK da tabela.';
 COMMENT ON COLUMN elmasri.funcionario.primeiro_nome IS 'Primeiro nome do funcionário.';
@@ -25,72 +26,77 @@ COMMENT ON COLUMN elmasri.funcionario.cpf_supervisor IS 'CPF do supervisor do fu
 COMMENT ON COLUMN elmasri.funcionario.numero_departamento IS 'Número do departamento do funcionário.';
 
 
-CREATE TABLE elmasri.departamento (
+CREATE TABLE elmasri.departamento
+             (
                 numero_departamento INTEGER NOT NULL,
                 nome_departamento VARCHAR(15) NOT NULL,
                 cpf_gerente CHAR(11) NOT NULL,
                 data_inicio_gerente DATE,
                 CONSTRAINT numero_departamento PRIMARY KEY (numero_departamento)
-);
+             );
 COMMENT ON TABLE elmasri.departamento IS 'Tabela que armazena as informações dos departamentos.';
 COMMENT ON COLUMN elmasri.departamento.numero_departamento IS 'Número do departamento. É a PK dessa tabela.';
 COMMENT ON COLUMN elmasri.departamento.nome_departamento IS 'Nome do departamento. Dado propriedades únicas através do index nome_departamento_idx.';
 COMMENT ON COLUMN elmasri.departamento.cpf_gerente IS 'CPF do gerente. É uma FK para a tabela funcionários.';
 COMMENT ON COLUMN elmasri.departamento.data_inicio_gerente IS 'Data do início do gerente no departamento.';
 
-
 CREATE UNIQUE INDEX nome_departamento_idx
  ON elmasri.departamento
  ( nome_departamento );
 
-CREATE TABLE elmasri.projeto (
+
+CREATE TABLE elmasri.projeto
+             (
                 numero_projeto INTEGER NOT NULL,
                 nome_projeto VARCHAR(15) NOT NULL,
                 local_projeto VARCHAR(15),
                 numero_departamento INTEGER NOT NULL,
                 CONSTRAINT numero_projeto_pk PRIMARY KEY (numero_projeto)
-);
+             );
 COMMENT ON TABLE elmasri.projeto IS 'Tabela que armazena as informações dos projetos dos departamentos.';
 COMMENT ON COLUMN elmasri.projeto.numero_projeto IS 'Número do projeto. É a PK da tabela.';
 COMMENT ON COLUMN elmasri.projeto.nome_projeto IS 'Nome do projeto. Dado propriedades únicas através do index alternate_index_projeto_idx.';
 COMMENT ON COLUMN elmasri.projeto.local_projeto IS 'Localização do projeto.';
 COMMENT ON COLUMN elmasri.projeto.numero_departamento IS 'Número do departamento. Serve como FK para a tabela departamento.';
 
-
 CREATE UNIQUE INDEX alternate_index_projeto_idx
  ON elmasri.projeto
  ( nome_projeto );
 
-CREATE TABLE elmasri.trabalha_em (
+
+CREATE TABLE elmasri.trabalha_em
+             (
                 cpf_funcionario CHAR(11) NOT NULL,
                 numero_projeto INTEGER NOT NULL,
                 horas NUMERIC(3,1) NOT NULL,
                 CONSTRAINT cpf_funcionario_pk PRIMARY KEY (cpf_funcionario, numero_projeto)
-);
+             );
 COMMENT ON TABLE elmasri.trabalha_em IS 'Tabela para armazenar quais funcionários trabalham em quais projetos.';
 COMMENT ON COLUMN elmasri.trabalha_em.cpf_funcionario IS 'CPF do funcionário. Faz parte da PK dessa tabela, e serve como FK para a tabela funcionário.';
 COMMENT ON COLUMN elmasri.trabalha_em.numero_projeto IS 'Número do projeto. Parte da PK dessa tabela, e é uma FK para a tabela projeto.';
 COMMENT ON COLUMN elmasri.trabalha_em.horas IS 'Horas trabalhadas pelo funcionário neste projeto.';
 
 
-CREATE TABLE elmasri.localizacoes_departamento (
+CREATE TABLE elmasri.localizacoes_departamento
+             (
                 numero_departamento INTEGER NOT NULL,
                 local VARCHAR(15) NOT NULL,
                 CONSTRAINT local_pk PRIMARY KEY (numero_departamento, local)
-);
+             );
 COMMENT ON TABLE elmasri.localizacoes_departamento IS 'Tabela que armazena as possíveis localizações dos departamentos.';
 COMMENT ON COLUMN elmasri.localizacoes_departamento.numero_departamento IS 'Número do departamento. Parte da PK dessa tabela, e serve de FK para a tabela departamento.';
 COMMENT ON COLUMN elmasri.localizacoes_departamento.local IS 'Localização do departamento. Parte da PK da tabela.';
 
 
-CREATE TABLE elmasri.dependente (
+CREATE TABLE elmasri.dependente
+             (
                 cpf_funcionario CHAR(11) NOT NULL,
                 nome_dependente VARCHAR(15) NOT NULL,
                 sexo CHAR(1),
                 data_nascimento DATE,
                 parentesco VARCHAR(15) NOT NULL,
                 CONSTRAINT nome_dependente_pk PRIMARY KEY (cpf_funcionario, nome_dependente)
-);
+             );
 COMMENT ON TABLE elmasri.dependente IS 'Tabela que armazena as informações dos dependentes dos funcionários.';
 COMMENT ON COLUMN elmasri.dependente.cpf_funcionario IS 'CPF do funcionário.Parte da PK da tabela, e também uma FK para a tabela funcionário.';
 COMMENT ON COLUMN elmasri.dependente.nome_dependente IS 'Nome do dependente. Parte da PK da tabela.';
